da399d76b7ec3621c39fc83fdefbc25307091a5b
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2020-09-17 23:20:50 -0300

    [CalcMedia] - Lógica
    sistema lógico implementado, com erro no feedback para usuario e nos calculos

--- app/src/main/java/com/example/gmwnotes/CalcMedia.java
+++ app/src/main/java/com/example/gmwnotes/CalcMedia.java
@@ -3,22 +3,95 @@ package com.example.gmwnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalcMedia extends AppCompatActivity {
 
-    final EditText av1text = findViewById(R.id.av1textfield);
-    final EditText av2text = findViewById(R.id.av2textfield);
+    final double corte = 7;
+    double av1Value = 0.0;
+    double av2Value = 0.0;
+    double av3Value = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_media);
-    }
-    private Double Calcular(){
-       Double resultado = 0.0;
-       Double faltante = 0.0;
 
-        return resultado;
+        final EditText av1text = findViewById(R.id.av1textfield);
+        final EditText av2text = findViewById(R.id.av2textfield);
+        final EditText av3text = findViewById(R.id.av3TextField);
+        final TextView faltantetext = findViewById(R.id.faltanteText);
+        final Button calcbutton = findViewById(R.id.calcButton);
+
+
+
+       calcbutton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               if(TextUtils.isEmpty(av1text.getText().toString()) && TextUtils.isEmpty(av2text.getText().toString())){
+
+                  if(!TextUtils.isEmpty(av3text.getText().toString())) {
+                       av1Value = Double.parseDouble(av1text.getText().toString());
+                       av2Value = Double.parseDouble(av2text.getText().toString());
+                   }else{
+                       av1Value = Double.parseDouble(av1text.getText().toString());
+                       av2Value = Double.parseDouble(av2text.getText().toString());
+                       av3Value = Double.parseDouble(av3text.getText().toString());
+                   }
+                   //Calcular(av1Value, av2Value, av3Value, faltantetext, av3text);
+               }
+               faltantetext.setText(Double.toString(Calcular(av1Value, av2Value, av3Value, faltantetext, av3text)));
+           }
+       });
+    }
+    private double Calcular(double av1Value, double av2Value, double av3Value, TextView afaltantetext, TextView av3text){
+       double resultado;
+       double faltante;
+       double maiorNota;
+       boolean temAv3 = false;
+
+
+        temAv3 = checkAv3(av3text);
+       maiorNota = getMaiorNota(temAv3, av1Value, av2Value, av3Value);
+
+       if(!temAv3){
+           resultado = (av1Value + av2Value)/2;
+           faltante = (corte*2)-maiorNota;
+       }else{
+           resultado = (av1Value + av2Value + av3Value)/3;
+           faltante = (corte*3)-maiorNota;
+       }
+
+       // faltantetext.setText("mamaedodoi");
+        return faltante;
+    }
+    private boolean checkAv3(TextView av3text){
+        if(!TextUtils.isEmpty(av3text.getText().toString())){
+            return false;
+        }else {
+            return true;
+        }
+    }
+    private double getMaiorNota(boolean _temAv3, double av1Value, double av2Value, double av3Value){
+        if(_temAv3){
+            if(av1Value > av2Value && av1Value > av3Value){
+                return av1Value;
+            }else if(av2Value > av1Value && av2Value > av3Value){
+                return  av2Value;
+            }else{
+                return av3Value;
+            }
+        }else{
+            if(av1Value > av2Value){
+                return av1Value;
+            }else{
+                return av2Value;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/gmwnotes/DashboardUser.java
+++ app/src/main/java/com/example/gmwnotes/DashboardUser.java
@@ -2,7 +2,10 @@ package com.example.gmwnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardUser extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class DashboardUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_user);
+
+        final Button calcButton = findViewById(R.id.calcButton);
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToCalcMedia();
+            }
+        });
+    }
+
+    private void goToCalcMedia(){
+        Intent intent = new Intent(this, CalcMedia.class);
+        startActivity(intent);
     }
 }
