8e484289be3a864051e47f85dc16e0a8548997eb
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2020-09-18 21:00:50 -0300

    [CalcMedia] - Lógica Finalizado
    Lógica da calculadora finalizado, faltando design e tratamento de feedback

--- app/src/main/java/com/example/gmwnotes/CalcMedia.java
+++ app/src/main/java/com/example/gmwnotes/CalcMedia.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalcMedia extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class CalcMedia extends AppCompatActivity {
     double av1Value = 0.0;
     double av2Value = 0.0;
     double av3Value = 0.0;
+    double finalResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,70 +28,86 @@ public class CalcMedia extends AppCompatActivity {
         final EditText av3text = findViewById(R.id.av3TextField);
         final TextView faltantetext = findViewById(R.id.faltanteText);
         final Button calcbutton = findViewById(R.id.calcButton);
+        calcbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                faltantetext.setText("");
+                if (!TextUtils.isEmpty(av1text.getText().toString()) && !TextUtils.isEmpty(av2text.getText().toString())) {
+                    if (TextUtils.isEmpty(av3text.getText().toString())) {
+                        av1Value = Double.parseDouble(av1text.getText().toString());
+                        av2Value = Double.parseDouble(av2text.getText().toString());
+                    } else {
+                        av1Value = Double.parseDouble(av1text.getText().toString());
+                        av2Value = Double.parseDouble(av2text.getText().toString());
+                        av3Value = Double.parseDouble(av3text.getText().toString());
+                    }
+                    finalResultado = Calcular(av1Value, av2Value, av3Value, faltantetext, av3text);
+                    if (finalResultado == 0) {
+                        faltantetext.setText("Você já passou!");
+                    } else {
+                        faltantetext.setText(Double.toString(finalResultado));
+                    }
+                }else{
+                    faltantetext.setText("Adicione os valores nos campos");
+                }
 
-
-
-       calcbutton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-
-               if(TextUtils.isEmpty(av1text.getText().toString()) && TextUtils.isEmpty(av2text.getText().toString())){
-
-                  if(!TextUtils.isEmpty(av3text.getText().toString())) {
-                       av1Value = Double.parseDouble(av1text.getText().toString());
-                       av2Value = Double.parseDouble(av2text.getText().toString());
-                   }else{
-                       av1Value = Double.parseDouble(av1text.getText().toString());
-                       av2Value = Double.parseDouble(av2text.getText().toString());
-                       av3Value = Double.parseDouble(av3text.getText().toString());
-                   }
-                   //Calcular(av1Value, av2Value, av3Value, faltantetext, av3text);
-               }
-               faltantetext.setText(Double.toString(Calcular(av1Value, av2Value, av3Value, faltantetext, av3text)));
-           }
-       });
+            }
+        });
     }
-    private double Calcular(double av1Value, double av2Value, double av3Value, TextView afaltantetext, TextView av3text){
-       double resultado;
-       double faltante;
-       double maiorNota;
-       boolean temAv3 = false;
 
+    private double Calcular(double av1Value, double av2Value, double av3Value, TextView faltantetext, TextView av3text) {
+        double resultado;
+        double faltante;
+        double maiorNota;
+        boolean temAv3 = false;
+        boolean passou = false;
 
         temAv3 = checkAv3(av3text);
-       maiorNota = getMaiorNota(temAv3, av1Value, av2Value, av3Value);
+        maiorNota = getMaiorNota(temAv3, av1Value, av2Value, av3Value);
 
-       if(!temAv3){
-           resultado = (av1Value + av2Value)/2;
-           faltante = (corte*2)-maiorNota;
-       }else{
-           resultado = (av1Value + av2Value + av3Value)/3;
-           faltante = (corte*3)-maiorNota;
-       }
-
-       // faltantetext.setText("mamaedodoi");
-        return faltante;
+        if (!temAv3) {
+            resultado = (av1Value + av2Value) / 2;
+        } else {
+            resultado = (av1Value + av2Value + av3Value) / 3;
+        }
+        faltante = (corte * 2) - maiorNota;
+        passou = checkPassou(resultado);
+        if (passou) {
+            return 0;
+        }else{
+            return faltante;
+        }
     }
-    private boolean checkAv3(TextView av3text){
-        if(!TextUtils.isEmpty(av3text.getText().toString())){
+
+    private boolean checkAv3(TextView av3text) {
+        if (TextUtils.isEmpty(av3text.getText().toString())) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
-    private double getMaiorNota(boolean _temAv3, double av1Value, double av2Value, double av3Value){
-        if(_temAv3){
-            if(av1Value > av2Value && av1Value > av3Value){
+
+    private boolean checkPassou(double resultado) {
+        if (resultado > corte) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private double getMaiorNota(boolean _temAv3, double av1Value, double av2Value, double av3Value) {
+        if (_temAv3) {
+            if (av1Value > av2Value && av1Value > av3Value) {
                 return av1Value;
-            }else if(av2Value > av1Value && av2Value > av3Value){
-                return  av2Value;
-            }else{
+            } else if (av2Value > av1Value && av2Value > av3Value) {
+                return av2Value;
+            } else {
                 return av3Value;
             }
-        }else{
-            if(av1Value > av2Value){
+        } else {
+            if (av1Value > av2Value) {
                 return av1Value;
-            }else{
+            } else {
                 return av2Value;
             }
         }
