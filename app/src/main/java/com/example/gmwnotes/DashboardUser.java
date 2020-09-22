3869d98d094c9639759e74a86ae67a6ce9ab8a67
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2020-09-21 21:01:46 -0300

    [Dashboard] - Layout
    Continuidade da Implementação da dashboard com Navigation Drawer

--- app/src/main/java/com/example/gmwnotes/DashboardUser.java
+++ app/src/main/java/com/example/gmwnotes/DashboardUser.java
@@ -1,31 +1,61 @@
 package com.example.gmwnotes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
-public class DashboardUser extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DashboardUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_user);
 
-        final Button calcButton = findViewById(R.id.calcButton);
 
-        calcButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToCalcMedia();
-            }
-        });
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                                this, drawerLayout,
+                                toolbar, (R.string.open),
+                                (R.string.close));
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
     }
 
     private void goToCalcMedia(){
         Intent intent = new Intent(this, CalcMedia.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+       if(menuItem.getItemId() == R.id.dashboard){
+
+        }
+        if(menuItem.getItemId() == R.id.calc){
+
+        }
+
+        return true;
     }
 }
