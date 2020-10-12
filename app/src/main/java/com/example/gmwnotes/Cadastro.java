27726309d3f397e09cce4881a6670cbebc28b699
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2020-10-12 11:06:10 -0300

    [Login]
    Começo do sistema de login utilizando Firebase

--- app/src/main/java/com/example/gmwnotes/Cadastro.java
+++ app/src/main/java/com/example/gmwnotes/Cadastro.java
@@ -5,30 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cadastro extends AppCompatActivity {
+
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userId;
+
+    TextView username;
+    TextView pass;
+    TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-        final Button cadbutton = findViewById(R.id.calcButton);
-       /* cadbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             //   openText();
-            }
-        });*/
+
+
+        username = findViewById(R.id.userField);
+        pass = findViewById(R.id.passField);
+        email = findViewById(R.id.emailField);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
     }
 
 
-        public void openText(){
-            String welcome = "Cadastrado com sucesso!";
-            Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, DashboardUser.class);
-            startActivity(intent);
+        public void cadastrar(){
+            userId = fAuth.getCurrentUser().getUid();
+            DocumentReference documentReference = fStore.collection("users").document(userId);
+            Map<String, Object> user = new HashMap<>();
+            user.put("username", username);
+            user.put("pass", pass);
+            user.put("email", email);
+            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d("TAG","sucesso");
+                }
+            });
         }
     }
